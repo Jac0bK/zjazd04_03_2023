1c6cee09f8ccc21722b57e432c72db973e5e8fe0
Jac0bK <j.kostansky@gmail.com>
2023-03-04 14:49:06 +0100

    obiektowa zadania

---
+++ strona_obiektowka.py
@@ -0,0 +1,14 @@
+from selenium import webdriver
+from obiektowka import LoginPage
+import time
+
+
+driver = webdriver.Chrome()
+page = LoginPage(driver)
+page.open()
+page.enter_username('standard_user')
+page.enter_password('secret_sauce')
+page.click_login()
+
+time.sleep(5)
+
